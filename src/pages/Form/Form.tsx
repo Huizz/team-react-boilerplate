8fe50a0cd4de3bcb1e6e6a25eb15e13139653e33
See Yihui <cuoguo@gmail.com>
2018-10-23 11:51:04 +0800

    Add pages folder

---
+++ src/pages/Form/Form.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import Button from 'components/Button';
+
+interface IProps {
+  startCount?: number
+}
+
+interface IState {
+  counter: number
+}
+
+class Form extends React.Component<IProps, IState> {
+
+  public state = {
+    counter: 1
+  }
+
+  public handleOnClick = () => {
+    this.setState({ counter: (this.state.counter + 1) })
+  }
+
+  public render = () => (
+    <div>
+      I have been clicked {this.state.counter} times!
+      <Button name="counter" size="large" onClick={this.handleOnClick} />
+    </div>
+  );
+
+}
+
+export default Form;
