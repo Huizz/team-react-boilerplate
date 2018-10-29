be5e05ae141daee15d1a13cc2e24fc39a4859b7a
See Yihui <cuoguo@gmail.com>
2018-10-29 17:58:40 +0800

    Add general helmet attributes to app

--- src/StaticApp.tsx
+++ src/StaticApp.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import { Route, Switch } from 'react-router-dom';
 
 import Form from 'pages/Form';
 import Home from 'pages/Home';
 
 const StaticApp: React.StatelessComponent<{}> = () => (
+  <>
   <Switch>
     <Route exact={true} path="/" component={Home} />
     <Route exact={true} path="/form" component={Form} />
   </Switch>
+  <Helmet>
+      <meta charSet="utf-8" />
+      <title>Demo app</title>
+    </Helmet>
+  </>
 )
 
 export default StaticApp;
