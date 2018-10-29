9e5f922d0dd978b7235b801bd99d51058da02a8a
See Yihui <cuoguo@gmail.com>
2018-10-29 15:47:58 +0800

    Edit server to run on koa

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,38 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
+
+// import * as express from 'express';
+import * as Koa from 'koa';
+import * as BodyParser from 'koa-bodyparser';
+import * as Router from 'koa-router';
+import * as serve from 'koa-static-server';
 import * as Loadable from 'react-loadable';
 
 import loader from './loader';
 
-const app = express();
+const app = new Koa();
 const port = process.env.PORT || 3003
+const router = new Router();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+router.get('*', loader)
 
-app.use(express.Router().get('/', loader));
+app.use(BodyParser({
+    enableTypes: ['json'],
+    jsonLimit: '5mb',
+    strict: true,
+    onerror(err, ctx) {
+        ctx.throw('body parse error', 422)
+    }
+}));
+
+// serve static files
+app.use(serve({ rootDir: 'build/static', rootPath: '/static' }))
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.use(loader);
+// app.use('express.static('build'))
 
 Loadable.preloadAll().then(() => {
-    app.listen(port, () => {console.log(`App listening on port ${port}!`)});
+    app.listen(port, () => { console.log(`App listening on port ${port}!`) });
 });
 
 // build the app first
--- server/loader.tsx
+++ server/loader.tsx
@@ -1,5 +1,5 @@
 // loads the react app
-
+import { Context } from 'koa';
 import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 // import Helmet from 'react-helmet';
@@ -13,7 +13,7 @@ import { state } from 'services/reducer';
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { StaticApp } from 'App';
+import StaticApp from 'StaticApp';
 
 const configureStore = () => {
     return createStore(
@@ -24,39 +24,43 @@ const configureStore = () => {
     )
 }
 
-const loader = (req : any, res : any) => {
-    const filePath = path.resolve('build/index.html'); // get compiled index page
-    fs.readFile(filePath, 'utf8', (err, htmlData) => {
-        if (err) {
-            console.error('read err', err)
-            return res.status(404).end()
-        }
-        const context = {
-            url: ''
-        }
-        const store = configureStore()
-        const markup = renderToString(
-            <Provider store={store}>
-                <StaticRouter
-                    location={req.url}
-                    context={context}
-                >
-                    <StaticApp />
-                </StaticRouter>
-            </Provider>
-        );
+const loader = async (ctx: Context, next: any) => {
+    await readFile(ctx); // readFile is async
+}
 
-        if (context.url) {
-            // Somewhere a `<Redirect>` was rendered
-            res.redirect(301, context.url)
-        } else {
-            // we're good, send the response
+const readFile = (ctx: Context) => {
+    return new Promise((resolve, reject) => {
+        const filePath = path.resolve('build/index.html'); // get compiled index page
+        fs.readFile(filePath, 'utf8', (err, htmlData) => {
+            if (err) {
+                console.error('read err', err)
+                ctx.status = 404
+                // @TODO: check if this is the correct method
+                reject(ctx);
+            }
+            const store = configureStore()
+            const staticContext = {};
+            const markup = renderToString(
+                <Provider store={store}>
+                    <StaticRouter
+                        location={ctx.url}
+                        context={staticContext}
+                    >
+                        <StaticApp />
+                    </StaticRouter>
+                </Provider>
+            );
+
+            // send the response
             const RenderedApp = htmlData.replace(
                 '<div id="root"></div>',
                 `<div id="root">${markup}</div><script>window.__PRELOADED_STATE__ = ${state}</script>`
-              );
-            res.send(RenderedApp)
-        }
+            );
+            ctx.status = 200;
+            ctx.type = 'html';
+            ctx.body = RenderedApp;
+            resolve(ctx);
+        })
     })
 }
- export default loader
+export default loader
