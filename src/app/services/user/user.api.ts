89d176a4b183d69c73b3d138a60f9f1a9c4e6087
See Yihui <cuoguo@gmail.com>
2018-10-31 11:12:06 +0800

    Add test username for demo

--- src/app/services/user/user.api.ts
+++ src/app/services/user/user.api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 // import * as dotenv from 'dotenv';
 
 const login = async (username: string, password: string): Promise<any> => {
+  // @TODO: remvoe testing value
+  if (!username) {
+    username = 'Banana';
+  }
   const userResponse = await axios.get(`${process.env.REACT_APP_API_URL}/v0/users/${username}`);
   const user = userResponse.data.body;
 
