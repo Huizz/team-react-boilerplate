1bf3fa4ed5a99b2271ece59db396181d404f4de5
See Yihui <cuoguo@gmail.com>
2018-10-31 10:19:45 +0800

    Add api url env variable

--- src/app/services/user/user.api.ts
+++ src/app/services/user/user.api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+// import * as dotenv from 'dotenv';
 
 const login = async (username: string, password: string): Promise<any> => {
-  const userResponse = await axios.get(`/v0/users/${username}`);
+  const userResponse = await axios.get(`${process.env.REACT_APP_API_URL}/v0/users/${username}`);
   const user = userResponse.data.body;
 
   return user;
